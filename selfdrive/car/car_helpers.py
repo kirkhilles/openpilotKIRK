b0e840a323071ee63697d0ddad2a8b360e5404c2
kirkhilles <kirk@hilles.net>
2020-10-17 17:47:55 -0400

    Corolla Value

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -168,6 +168,9 @@ def get_car(logcan, sendcan, has_relay=False):
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
     candidate = "mock"
+    
+    #KRH
+    candidate = "TOYOTA COROLLA TSS2 2019"
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
